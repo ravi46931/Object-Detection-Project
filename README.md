89f4fab3eb2d9d6809a9f4de959356db3442f513
Ravi Kumar <ravikumar46931@gmail.com>
2024-03-06 19:54:47 +0530

    data collector

---
+++ README.md
@@ -0,0 +1,12 @@
+# Object Detection Project
+
+## Implementation
+ - Create a vrtual environment
+ ```bash
+ python -m venv objectenv
+ ```
+- Activate virtual environment
+- Install requirements
+
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Ravi Kumar',
+    author_email= 'ravikumar46931@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
